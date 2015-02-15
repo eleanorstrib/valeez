80bf78f44ff78fca34228ca1edc9365ad04ec704
eleanorstrib <eleanor@sloan.mit.edu>
2015-02-15 00:15:56 -0800

    Several additions: jQuery datepicker added with some defaults -- next step: add restrictions Dropdowns styled with square corners, font from style guide Button restyled with font from style guide and square corners

--- static/script.js
+++ static/script.js
@@ -1,10 +1,7 @@
-$(document).ready(function{
-	var minDate = $( ".selector" ).datepicker( "option", "minDate" );
-	var maxDate = $( ".selector" ).datepicker( "option", "maxDate" );
-	$( ".selector" ).datepicker({ minDate: "+0d" });
-    $( ".selector" ).datepicker({ maxDate: "+10d" });
-
-
-
-
-});
+  $(function() {
+    $( "#depart-date, #return-date" ).datepicker({
+        defaultDate: "+1w",
+        maxDate: "+10d",
+    
+        });
+  });
